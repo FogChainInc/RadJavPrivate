715dda8a0c65dbe02d258d66764439cf94b76a04
Nathanael Coonrod <ncoonrod@highersoftware.com>
2018-03-20 00:03:12 -0700

    Networking examples

---
+++ examples/webServer/webServer.js
@@ -0,0 +1,16 @@
+var webServer = new RadJav.Net.WebServer ();
+
+webServer.serve (function (request)
+	{
+		var response = "";
+
+		if (request.type == RadJav.Net.WebServerRequestType.GET)
+		{
+			if (request.page == "/")
+				response = "Hello world!";
+		}
+
+		return (response);
+	});
+
+webServer.listen (80);
