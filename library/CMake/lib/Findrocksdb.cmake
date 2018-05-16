86a29ed1eff0425c598b1e58c300d3a4404daeb4
Nathanael Coonrod <ncoonrod@highersoftware.com>
2018-05-16 05:03:06 -0700

    CMake RocksDB Fix

--- library/CMake/lib/Findrocksdb.cmake
+++ library/CMake/lib/Findrocksdb.cmake
@@ -1,13 +1,11 @@
 message (STATUS "Searching for RocksDB...")
 
-if (NOT WIN32)
-	set (rocksdb_SOURCE $ENV{rocksdb_SOURCE} CACHE PATH "rocksdb path")
-	fixPath (rocksdb_SOURCE)
-	set (rocksdb_SEARCH_PATHS ${rocksdb_SOURCE} ${RADJAV_DEPENDENCIES})
+set (rocksdb_SOURCE $ENV{rocksdb_SOURCE} CACHE PATH "rocksdb path")
+fixPath (rocksdb_SOURCE)
+set (rocksdb_SEARCH_PATHS ${rocksdb_SOURCE} ${RADJAV_DEPENDENCIES})
 
-	searchForHeader (rocksdb rocksdb/db.h "${rocksdb_SEARCH_PATHS}" FALSE)
-	searchForLibrary (rocksdb rocksdb rocksdb "${rocksdb_SEARCH_PATHS}")
+searchForHeader (rocksdb rocksdb/db.h "${rocksdb_SEARCH_PATHS}" FALSE)
+searchForLibrary (rocksdb rocksdb rocksdb "${rocksdb_SEARCH_PATHS}")
 
-	hasLibBeenFound (rocksdb)
-endif ()
+hasLibBeenFound (rocksdb)
 
