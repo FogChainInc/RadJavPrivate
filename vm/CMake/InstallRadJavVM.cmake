8cc4abde795de151c6753bbe97a01b3c1052ad22
Mikalai Turankou <mturankou@exadel.com>
2018-06-01 11:59:58 +0300

    Mac OSX bundle generation bits - Added code signing possibility - Info.plist.in moved into CMake folder as a template - install target only deal with bundle fixing and code signing - Added RadJavVMLauncher Objective-C app which will handle openFile Apple event and create a real VM process with .xrj file to launch - Updated DS_Store.in to use new Volume name

--- vm/CMake/InstallRadJavVM.cmake
+++ vm/CMake/InstallRadJavVM.cmake
@@ -1,45 +1,17 @@
 
 if (APPLE)
-	set (RadJavVM_DEPENDENCY_OBJECTS_LIST "./natives_blob.bin;./snapshot_blob.bin" CACHE STRING "Semicolon separated list of paths to dependencies \
-												which are indirectly will be used by application, i.e. loading \
-												during runtime")
-	
-	#Configuring Info.plist for Mac Bundle
-	set (RadJavVM_BUNDLE_DISPLAY_NAME ${CMAKE_PROJECT_NAME})
-	set (RadJavVM_BUNDLE_SIGNATURE "rjvm")
-	
-	set_target_properties (${CMAKE_PROJECT_NAME} PROPERTIES MACOSX_BUNDLE_INFO_PLIST ${CMAKE_SOURCE_DIR}/installers/mac/Info.plist.in)
-	set_target_properties (${CMAKE_PROJECT_NAME} PROPERTIES MACOSX_BUNDLE_GUI_IDENTIFIER "com.highersoftware.${CMAKE_PROJECT_NAME}")
-	set_target_properties (${CMAKE_PROJECT_NAME} PROPERTIES MACOSX_BUNDLE_BUNDLE_NAME ${CMAKE_PROJECT_NAME})
-	set_target_properties (${CMAKE_PROJECT_NAME} PROPERTIES MACOSX_BUNDLE_BUNDLE_VERSION ${PROJECT_VERSION})
-	set_target_properties (${CMAKE_PROJECT_NAME} PROPERTIES MACOSX_BUNDLE_LONG_VERSION_STRING ${PROJECT_VERSION})
-	set_target_properties (${CMAKE_PROJECT_NAME} PROPERTIES MACOSX_BUNDLE_SHORT_VERSION_STRING ${PROJECT_VERSION})
-	set_target_properties (${CMAKE_PROJECT_NAME} PROPERTIES MACOSX_BUNDLE_ICON_FILE "RadJav.icns")
-	set_target_properties (${CMAKE_PROJECT_NAME} PROPERTIES MACOSX_BUNDLE_COPYRIGHT "© 2018, Higher Edge Software")
-	
-	#Copy specific files into a bundle
-	set (APP_BUNDLE_LOCATION "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${CMAKE_PROJECT_NAME}.app")
-	set (APP_EXECUTABLE_DIR "${APP_BUNDLE_LOCATION}/Contents/MacOS")
-	set (APP_RESOURCES_DIR "${APP_BUNDLE_LOCATION}/Contents/Resources")
-	
-	foreach(dependency ${RadJavVM_DEPENDENCY_OBJECTS_LIST})
-		add_custom_command (TARGET ${CMAKE_PROJECT_NAME} POST_BUILD 
-					COMMAND ${CMAKE_COMMAND} -E copy ${dependency} ${APP_EXECUTABLE_DIR}
-					COMMENT "Adding extra dependency into application bundle: ${dependency}"
-					VERBATIM )
-	endforeach(dependency)
-	
-	
-	#Copy icons
-	install (FILES ${CMAKE_SOURCE_DIR}/installers/mac/RadJav.icns DESTINATION ${CMAKE_PROJECT_NAME}.app/Contents/Resources)
-	
 	#Correct binary files to be able to execute on client side
 	install (CODE "
 			include(BundleUtilities)
-			fixup_bundle(\"${APP_BUNDLE_LOCATION}\" \"\" \"\")
+			fixup_bundle(\"${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${CMAKE_PROJECT_NAME_LAUNCHER}.app\" \"\" \"\")
 			"
 		COMPONENT Runtime)
-
+	
+	#Signing
+	install (CODE 
+		"execute_process(COMMAND codesign --force --deep --verbose --verify --sign \"${MAC_CODESIGN_IDENTITY}\" ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${CMAKE_PROJECT_NAME_LAUNCHER}.app)")
+	
 	#Installing as app bundle
-	install(TARGETS ${CMAKE_PROJECT_NAME} BUNDLE DESTINATION . )
+	install (TARGETS ${CMAKE_PROJECT_NAME_LAUNCHER} BUNDLE DESTINATION . )
+	
 endif()
--- vm/CMake/PackageRadJavVM.cmake
+++ vm/CMake/PackageRadJavVM.cmake
@@ -11,23 +11,23 @@ set (CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
 
 set (CPACK_RPM_PACKAGE_DEBUG 0)
 
-if(MSVC)
+if (MSVC)
     set (CPACK_GENERATOR "NSIS")
 endif(MSVC)
 
-if(APPLE)
+if (APPLE)
 	set (CPACK_GENERATOR "DragNDrop")
 	
-	set (CPACK_PACKAGE_FILE_NAME ${CMAKE_PROJECT_NAME}-${CPACK_PACKAGE_VERSION})
+	set (CPACK_PACKAGE_FILE_NAME ${CMAKE_PROJECT_NAME_LAUNCHER}-${CPACK_PACKAGE_VERSION})
 	set (CPACK_PACKAGE_ICON ${CMAKE_SOURCE_DIR}/installers/mac/RadJav.icns)
 	
 	set (CPACK_DMG_DS_STORE "${CMAKE_SOURCE_DIR}/installers/mac/DS_Store.in")
 	set (CPACK_DMG_BACKGROUND_IMAGE "${CMAKE_SOURCE_DIR}/installers/mac/DmgBackground.png")
 	
 	set (CPACK_DMG_FORMAT "UDBZ")
-	set (CPACK_DMG_VOLUME_NAME ${CMAKE_PROJECT_NAME})
+	set (CPACK_DMG_VOLUME_NAME ${CMAKE_PROJECT_NAME_LAUNCHER})
 	
-endif(APPLE)
+endif (APPLE)
 
 if (${CMAKE_SYSTEM_NAME} MATCHES Linux)
 	find_program(RPM_EXE rpmbuild)
@@ -37,7 +37,7 @@ if (${CMAKE_SYSTEM_NAME} MATCHES Linux)
 	else()
 	    set(CPACK_GENERATOR "TGZ;DEB;RPM")
 	endif()
-endif(${CMAKE_SYSTEM_NAME} MATCHES Linux)
+endif (${CMAKE_SYSTEM_NAME} MATCHES Linux)
 
 # Debian specific configuration (minimum)
 #set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_CONTACT}")
@@ -52,3 +52,8 @@ endif(${CMAKE_SYSTEM_NAME} MATCHES Linux)
 
 include (InstallRequiredSystemLibraries)
 include (CPack)
+
+#Signing installation image
+#if (APPLE)
+#	execute_process (COMMAND codesign --force --deep --verbose --verify --sign "${MAC_CODESIGN_IDENTITY}" "${CMAKE_CURRENT_BINARY_DIR}/${CPACK_PACKAGE_FILE_NAME}.dmg")
+#endif ()
