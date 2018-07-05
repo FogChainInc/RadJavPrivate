4f009376cb5b256a41dc7bbc19346244e503d89f
Mikalai Turankou <mturankou@exadel.com>
2018-07-05 09:27:41 +0300

    Added cmake script to find ColladaLoader shared library which will be included in libRadJav

---
+++ library/CMake/lib/FindColladaLoader.cmake
@@ -0,0 +1,15 @@
+message (STATUS "Searching for Collada Loader...")
+
+set (COLLADA_SOURCE $ENV{COLLADALOADER_HOME} CACHE PATH "Path to OgreColladaLoader library install directory")
+fixPath (COLLADA_SOURCE)
+set (COLLADA_SEARCH_PATHS ${COLLADA_SOURCE} ${RADJAV_DEPENDENCIES})
+
+searchForHeader (COLLADA OgreColladaLoader.h ${COLLADA_SEARCH_PATHS} FALSE)
+
+if (APPLE)
+	searchForLibrary (COLLADA ogrecolladaloader ogrecolladaloader ${COLLADA_SEARCH_PATHS})
+else ()
+	searchForLibrary (COLLADA ogrecolladaloader ogrecolladaloader ${COLLADA_SEARCH_PATHS})
+endif ()
+
+hasLibBeenFound (COLLADA)
