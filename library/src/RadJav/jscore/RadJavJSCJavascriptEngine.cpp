ff8828de21eaef46965796f7ff25b8a457d3efe2
Andrey Bolshakov <abolshakov@exadel.com>
2018-09-27 16:20:11 +0300

    Added delay to run loop.

--- library/src/RadJav/jscore/RadJavJSCJavascriptEngine.cpp
+++ library/src/RadJav/jscore/RadJavJSCJavascriptEngine.cpp
@@ -532,7 +532,7 @@ namespace RadJAV
                 else {
                     
                 #ifdef USE_IOS
-                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.03 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                         runApplicationSingleStep();
                     });
                 #endif
