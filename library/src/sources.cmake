114839ee58df5dcbda9df1d0400512f35e84de8e
Mikalai Turankou <mturankou@exadel.com>
2018-09-27 14:26:10 +0300

    Added draft MUI Alert implementation Extended C API of RadJav library with 	RadJav_showError method

--- library/src/sources.cmake
+++ library/src/sources.cmake
@@ -480,7 +480,8 @@ if (IS_MOBILE)
 	if (IOS)
 		set (SOURCES_files_RadJav__cpp__MUI__Sources 
 			"${libRadJav_SOURCE_DIR}/src/RadJav/cpp/ios/RadJavCPPMUIButtonFrame.mm"
-			"${libRadJav_SOURCE_DIR}/src/RadJav/cpp/ios/RadJavCPPMUIViewFrame.mm")
+			"${libRadJav_SOURCE_DIR}/src/RadJav/cpp/ios/RadJavCPPMUIViewFrame.mm"
+			"${libRadJav_SOURCE_DIR}/src/RadJav/cpp/ios/RadJavCPPMUIAlertFrame.mm")
 
 		source_group ("RadJav\\CPP\\MUI\\iOS" FILES ${SOURCES_files_RadJav__cpp__MUI__Sources})
 		set (SOURCES ${SOURCES_files_RadJav__cpp__MUI__Sources} ${SOURCES})
