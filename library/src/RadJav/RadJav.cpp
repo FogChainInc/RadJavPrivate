25688425c558445a91f4a1d35bb002ace414ae1d
Mikalai Turankou <mturankou@exadel.com>
2018-09-28 10:47:20 +0300

    Fixed compile error for non Android platforms

--- library/src/RadJav/RadJav.cpp
+++ library/src/RadJav/RadJav.cpp
@@ -75,10 +75,10 @@
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #else
-#define  LOGE()
-#define  LOGW()
-#define  LOGD()
-#define  LOGI()
+#define  LOGE(...)
+#define  LOGW(...)
+#define  LOGD(...)
+#define  LOGI(...)
 #endif
 
 #ifdef RADJAV_DEBUG
