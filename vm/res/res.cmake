47d3cd7fa56d6e16187965873b7385e3e969da24
Andrey Bolshakov <abolshakov@exadel.com>
2018-10-02 18:25:27 +0300

    Reverted some questionable changes in black stripes fix.

--- vm/res/res.cmake
+++ vm/res/res.cmake
@@ -1,5 +1,12 @@
+if (IOS)
 set (SOURCES_files_RadJavVM__Resources
 	"${RadJavVM_SOURCE_DIR}/res/LaunchScreen.storyboard")
+endif ()
+if (!IOS)
+set (SOURCES_files_RadJavVM__Resources
+	"${RadJavVM_SOURCE_DIR}/res/RadJavVM.rc")
+endif ()
+
 source_group ("RadJavVM\\Resources" FILES ${SOURCES_files_RadJavVM__Resources})
 
 set (RadJavVM_SOURCES ${SOURCES_files_RadJavVM__Resources} ${RadJavVM_SOURCES})
