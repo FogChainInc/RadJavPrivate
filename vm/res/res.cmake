c06b7637776a9cce37007264e462b83a08f7d7ce
Andrey Bolshakov <abolshakov@exadel.com>
2018-10-02 15:01:03 +0300

    Fixed issue with black stripes on top/bottom (app running in iPhone 4 compatibility mode). LaunchScreen.storyboard can be used to set up initial splash screen.

--- vm/res/res.cmake
+++ vm/res/res.cmake
@@ -1,5 +1,5 @@
 set (SOURCES_files_RadJavVM__Resources
-	"${RadJavVM_SOURCE_DIR}/res/RadJavVM.rc")
+	"${RadJavVM_SOURCE_DIR}/res/LaunchScreen.storyboard")
 source_group ("RadJavVM\\Resources" FILES ${SOURCES_files_RadJavVM__Resources})
 
 set (RadJavVM_SOURCES ${SOURCES_files_RadJavVM__Resources} ${RadJavVM_SOURCES})
