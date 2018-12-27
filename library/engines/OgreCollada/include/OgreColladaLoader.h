8b7985a10eccf4141dc52b47f84fc2a90684befa
Mikalai Turankou <mturankou@exadel.com>
2018-12-27 16:25:20 +0300

    Portion of code documentation

--- library/engines/OgreCollada/include/OgreColladaLoader.h
+++ library/engines/OgreCollada/include/OgreColladaLoader.h
@@ -12,6 +12,9 @@ namespace Ogre
 }
 
 namespace Ogre {
+	/**
+	 * @brief Class which handle import of \b COLLADA files.
+	 */
 	class OCL_EXPORT ColladaLoader
 	{
 	public:
@@ -19,6 +22,15 @@ namespace Ogre {
 		ColladaLoader(const ColladaLoader& other) = delete;
 
 	public:
+		/**
+		 * @brief Method to load \b COLLADA object into scene.
+		 * @details Use this method to load \b COLLADA objects from files and attach them into scene node.
+		 * @param[in] daeFilePath Absolute path to COLLADA(\c .dae) file.
+		 * @param[in] sceneManager Pointer to current scene manager which manage 3D objects.
+		 * @param[in] parentNode Pointer to parent node to which loaded 3D object will be attached.
+		 * @param[in] resourcesPath Absolute path to folder where additional resources reside, for example textures used in 3D model.
+		 * @exception ERR_INTERNAL_ERROR If error occur during file loading process.
+		 */
 		static void load( const Ogre::String& daeFilePath,
 						 Ogre::SceneManager* sceneManager,
 						 Ogre::SceneNode* parentNode,
--- library/include/RadJav/RadJavJSExternals.h
+++ library/include/RadJav/RadJavJSExternals.h
@@ -40,41 +40,65 @@ namespace RadJAV
 	}
 	
 	/**
-	 * Main class for managing external C++ objects within withing Javascript engine.
+	 * @brief Main class for managing external C++ objects within Javascript engine.
+	 * @details Currently this class supports two types of Javascript engines(V8, JavaScriptCore). The idea behind this class is to
+	 * manage external data within Javascript objects. For V8 it create new Persistent object where we place C++ pointer as external data
+	 * and subscribe to garbage collector callback to be notified when Javascript object no longer in use. When garbage collector callback
+	 * is triggered we delete underlying C++ object. If C++ object goes out of scope(deleted from C++ side) there is ability to clear
+	 * corresponding Persistent Javascript object. In case of JavaScriptCore engine we create C++ object wrapper which exposed to
+	 * Javascript side and which will hold pointer to our specific C++ object. Garbage collector in that case will call our hook on
+	 * finalize callback when Javascript object is no longer in use and that will lead to C++ object destruction. If C++ object goes
+	 * out of scope(deleted from C++ side) there is ability to clear corresponding Javascript wrapper object.
+	 * 
+	 * User has two ways to export C++ objects.
+	 * @li Subclassing CPP::ChainedPtr, which has the ability to notify on object destruction.
+	 * @li \c std::shared_ptr<T>, reference to which will be reset when Javascript object will go out of scope.
+	 * 
+	 * @note Usually user will create one instance of this ExternalsManager and will use it widely in his code.
 	 */
 	class ExternalsManager
 	{
 	public:
 		/**
-		 * Contructor.
+		 * @brief Contructor.
 		 */
 		ExternalsManager();
 
 		/**
-		 * Destructor.
+		 * @brief Destructor.
+		 * @details If during ExternalsManager destruction there are still exposed C++ objects which didn't freed we clear them directly.
 		 */
 		~ExternalsManager();
 		
+		/**
+		 * @brief Copy consctructor.
+		 * @details We didn't expect to everyone use multiple instances for this ExternalManager so this constructor was removed.
+		 */
 		ExternalsManager(const ExternalsManager& ) = delete;
+		
+		/**
+		 * @brief Assignment operator.
+		 * @details We didn't expect to everyone use multiple instances for this ExternalManager so this assignment operator was removed.
+		 */
 		ExternalsManager& operator = (const ExternalsManager&) = delete;
 
 #ifdef USE_V8
 		/**
-		 * Add relation between C++ object and corresponding Javascript object field.
-		 * Object will be destroyed during Javascript engine garbage collector notification or directly from C++ side.
-		 * @param handle is a context to which we plan to add new C++ object.
-		 * @param functionName a name of the property of Javascript object to bind external object with.
-		 * @param object a pointer to external object derived from ChainedPtr class.
+		 * @brief Add relation between C++ object and corresponding Javascript object field.
+		 * @details Object will be destroyed during Javascript engine garbage collector notification or directly from C++ side.
+		 * @param[in] handle is a context to which we plan to add new C++ object.
+		 * @param[in] functionName a name of the property of Javascript object to bind external object with.
+		 * @param[in] object a pointer to external object derived from ChainedPtr class.
 		 */
 		void set(const v8::Local<v8::Object>& handle, const String& functionName, CPP::ChainedPtr* object);
 		
 		/**
-		 * Template function to add a relation between C++ object and corresponding Javascript object field.
-		 * Object will be reset during Javascript garbage collector callback or
-		 *  when ExternalsManager will go out of scope.
-		 * @param handle is a context to which we plan to add new C++ object.
-		 * @param functionName a name of the property of Javascript object to bind external object with.
-		 * @param object a shared pointer to external object.
+		 * @brief Template function to add a relation between C++ object and corresponding Javascript object field.
+		 * @details Object will be reset during Javascript garbage collector callback or
+		 * when ExternalsManager will go out of scope.
+		 * @param[in] handle is a context to which we plan to add new C++ object.
+		 * @param[in] functionName a name of the property of Javascript object to bind external object with.
+		 * @param[in] object a shared pointer to external object.
 		 */
 		template<class T>
 		void set(const v8::Local<v8::Object>& handle, const String& functionName, std::shared_ptr<T> object)
@@ -83,21 +107,21 @@ namespace RadJAV
 		}
 		
 		/**
-		 * Get external object from Javascript object's field.
-		 * If C++ external object is out of scope function will return nullptr.
-		 * @param handle is a context(Javascript object) for which we query.
-		 * @param functionName a name of the property of Javascript object from which to retrieve
+		 * @brief Get external object from Javascript object's field.
+		 * @details If C++ external object is out of scope function will return nullptr.
+		 * @param[in] handle is a context(Javascript object) for which we query.
+		 * @param[in] functionName a name of the property of Javascript object from which to retrieve
 		 *  external object.
 		 * @return ChainedPtr a pointer to ChainedPtr derived class which where stored by set().
 		 */
 		CPP::ChainedPtr* get(const v8::Local<v8::Object>& handle, const String& functionName);
 		
 		/**
-		 * Template function to get external object from Javascript object's field.
-		 * If C++ external object where freed by garbage collector callback then
+		 * @brief Template function to get external object from Javascript object's field.
+		 * @details If C++ external object where freed by garbage collector callback then
 		 * function will return empty shared pointer.
-		 * @param handle is a context(Javascript object) for which we query.
-		 * @param functionName a name of the property of Javascript object from which to retrieve
+		 * @param[in] handle is a context(Javascript object) for which we query.
+		 * @param[in] functionName a name of the property of Javascript object from which to retrieve
 		 *  external object.
 		 * @return shared_ptr<T> a shared pointer to arbitrary C++ object previously stored by set().
 		 */
@@ -108,36 +132,36 @@ namespace RadJAV
 		}
 		
 		/**
-		 * Clear Javascript object's field which hold external object ID.
-		 * Underlying C++ object will not be freed/reset during that call.
-		 *  Subsequent calls to get() for this functionName will return nullptr.
-		 * @param handle is a context(Javascript object) for which we clear external object ID.
-		 * @param functionName a name of the property of Javascript object from which to retrieve
-		 *  external object.
+		 * @brief Clear Javascript object's field which hold external object ID.
+		 * @details Underlying C++ object will not be freed/reset during that call.
+		 * Subsequent calls to get() for this functionName will return nullptr.
+		 * @param[in] handle is a context(Javascript object) for which we clear external object ID.
+		 * @param[in] functionName a name of the property of Javascript object from which to retrieve
+		 * external object.
 		 * @note function is usefull in cases where we trying to delete wrapped C++ object from
-		 *  C++ side, lets say by destroy() function which were called from Javascript side.
+		 * C++ side, lets say by destroy() function which were called from Javascript side.
 		 */
 		void clear(const v8::Local<v8::Object>& handle, const String& functionName);
 
 #elif defined USE_JAVASCRIPTCORE
 		/**
-		 * Add relation between C++ object and corresponding Javascript object field.
-		 * Object will be destroyed during Javascript engine garbage collector notification or directly from C++ side.
-		 * @param context is a context of Javascript virtual machine.
-		 * @param handle is a Javascript object to which we plan to add new C++ object.
-		 * @param functionName a name of the property of Javascript object to bind external object with.
-		 * @param object a pointer to external object derived from ChainedPtr class.
+		 * @brief Add relation between C++ object and corresponding Javascript object field.
+		 * @details Object will be destroyed during Javascript engine garbage collector notification or directly from C++ side.
+		 * @param[in] context is a context of Javascript virtual machine.
+		 * @param[in] handle is a Javascript object to which we plan to add new C++ object.
+		 * @param[in] functionName a name of the property of Javascript object to bind external object with.
+		 * @param[in] object a pointer to external object derived from ChainedPtr class.
 		 */
 		void set(JSContextRef context, JSObjectRef handle, const String& functionName, CPP::ChainedPtr* object);
 		
 		/**
-		 * Template function to add a relation between C++ object and corresponding Javascript object field.
-		 * Object will be reset during Javascript garbage collector callback or
-		 *  when ExternalsManager will go out of scope.
-		 * @param context is a context of Javascript virtual machine.
-		 * @param handle is a Javascript object to which we plan to add new C++ object.
-		 * @param functionName a name of the property of Javascript object to bind external object with.
-		 * @param object a shared pointer to external object.
+		 * @brief Template function to add a relation between C++ object and corresponding Javascript object field.
+		 * @details Object will be reset during Javascript garbage collector callback or
+		 * when ExternalsManager will go out of scope.
+		 * @param[in] context is a context of Javascript virtual machine.
+		 * @param[in] handle is a Javascript object to which we plan to add new C++ object.
+		 * @param[in] functionName a name of the property of Javascript object to bind external object with.
+		 * @param[in] object a shared pointer to external object.
 		 */
 		template<class T>
 		void set(JSContextRef context, JSObjectRef handle, const String& functionName, std::shared_ptr<T> object)
@@ -146,24 +170,24 @@ namespace RadJAV
 		}
 		
 		/**
-		 * Get external object from Javascript object's field.
-		 * If C++ external object is out of scope function will return nullptr.
-		 * @param context is a context of Javascript virtual machine.
-		 * @param handle is a Javascript object from which we get C++ object.
-		 * @param functionName a name of the property of Javascript object from which to retrieve
-		 *  external object.
+		 * @brief Get external object from Javascript object's field.
+		 * @details If C++ external object is out of scope function will return nullptr.
+		 * @param[in] context is a context of Javascript virtual machine.
+		 * @param[in] handle is a Javascript object from which we get C++ object.
+		 * @param[in] functionName a name of the property of Javascript object from which to retrieve
+		 * external object.
 		 * @return ChainedPtr a pointer to ChainedPtr derived class which where stored by set().
 		 */
 		CPP::ChainedPtr* get(JSContextRef context, JSObjectRef handle, const String& functionName);
 		
 		/**
-		 * Template function to get external object from Javascript object's field.
-		 * If C++ external object where freed by garbage collector callback then
+		 * @brief Template function to get external object from Javascript object's field.
+		 * @details If C++ external object where freed by garbage collector callback then
 		 * function will return empty shared pointer.
-		 * @param context is a context of Javascript virtual machine.
-		 * @param handle is a Javascript object from which we get C++ object.
-		 * @param functionName a name of the property of Javascript object from which to retrieve
-		 *  external object.
+		 * @param[in] context is a context of Javascript virtual machine.
+		 * @param[in] handle is a Javascript object from which we get C++ object.
+		 * @param[in] functionName a name of the property of Javascript object from which to retrieve
+		 * external object.
 		 * @return shared_ptr<T> a shared pointer to arbitrary C++ object previously stored by set().
 		 */
 		template<class T>
@@ -173,15 +197,15 @@ namespace RadJAV
 		}
 		
 		/**
-		 * Clear Javascript object's field which hold external object ID.
-		 * Underlying C++ object will not be freed/reset during that call.
-		 *  Subsequent calls to get() for this functionName will return nullptr.
-		 * @param context is a context of Javascript virtual machine.
-		 * @param handle is a Javascript object for which we get clear object ID related to C++ object.
-		 * @param functionName a name of the property of Javascript object from which to retrieve
-		 *  external object.
+		 * @brief Clear Javascript object's field which hold external object ID.
+		 * @details Underlying C++ object will not be freed/reset during that call.
+		 * Subsequent calls to get() for this functionName will return nullptr.
+		 * @param[in] context is a context of Javascript virtual machine.
+		 * @param[in] handle is a Javascript object for which we get clear object ID related to C++ object.
+		 * @param[in] functionName a name of the property of Javascript object from which to retrieve
+		 * external object.
 		 * @note function is usefull in cases where we trying to delete wrapped C++ object from
-		 *  C++ side, lets say by destroy() function which were called from Javascript side.
+		 * C++ side, lets say by destroy() function which were called from Javascript side.
 		 */
 		void clear(JSContextRef context, JSObjectRef handle, const String& functionName);
 #endif
