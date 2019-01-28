c1e3757fa453374c08c4b5a9d193aa6ea831b4f3
Nathanael Coonrod <ncoonrod@highersoftware.com>
2019-01-28 05:49:03 -0800

    Bindergen fixes

--- library/gen/RadJav.GUI.GObject.js
+++ library/gen/RadJav.GUI.GObject.js
@@ -2,22 +2,25 @@ module.exports = async function (Bindergen)
 {
 	var GeneratorClass = require ("./GeneratorClass").GeneratorClass;
 
-	let embed = Bindergen.useGenerators (["RadJavGenerator", "V8"]);
-	//embed.appendToOutput ("cmakeV8Includes", "${libRadJav_SOURCE_DIR}/include/RadJav/v8/RadJavV8GUIGObject.h");
-	let RadJavGUI = embed.useNamespace ("RadJav.GUI");
-
-	class GObject extends GeneratorClass
+	if (Bindergen.hasGenerator ("V8") == true)
 	{
-		constructor ()
-		{
-			super ("GObject");
-			this._appObj = { type: "external" };
-		}
-	}
+		let embed = Bindergen.useGenerators (["RadJavGenerator", "V8"]);
+		//embed.appendToOutput ("cmakeV8Includes", "${libRadJav_SOURCE_DIR}/include/RadJav/v8/RadJavV8GUIGObject.h");
+		let RadJavGUI = embed.useNamespace ("RadJav.GUI");
 
-	let classObj = await RadJavGUI.createClass ("GObject", GObject);
-	let outputBlock = classObj.generateOutputBlock ({ type: "cppHeader" });
-	//embed.createFile ("library/include/RadJav/v8/RadJavCPPGUIGObject2.h", [outputBlock]);
+		class GObject extends GeneratorClass
+		{
+			constructor ()
+			{
+				super ("GObject");
+				this._appObj = { type: "external" };
+			}
+		}
+
+		let classObj = await RadJavGUI.createClass ("GObject", GObject);
+		let outputBlock = classObj.generateOutputBlock ({ type: "cppHeader" });
+		//embed.createFile ("library/include/RadJav/v8/RadJavCPPGUIGObject2.h", [outputBlock]);
+	}
 
 	let generators = Bindergen.useGenerators (["AndroidJNI", "RadJavGenerator"]);
 	generators.parseFiles ([
--- library/gen/RadJav.MUI.Button.js
+++ library/gen/RadJav.MUI.Button.js
@@ -1,65 +1,68 @@
 module.exports = async function (Bindergen)
 {
-	let embed = Bindergen.useGenerators (["RadJavGenerator", "V8"]);
-	//embed.appendToCustomFile ("cmakeV8Includes", "${libRadJav_SOURCE_DIR}/include/RadJav/v8/RadJavV8GUIButton.h");
-	//embed.outputFilename ("RadJav%PASS%GUIButton");
-	let RadJavGUI = embed.useNamespace ("RadJav.GUI");
-	let GObject = RadJavGUI.getClass ("GObject");
-
-	class Button extends GObject
+	if (Bindergen.hasGenerator ("V8") == true)
 	{
-		constructor ()
+		let embed = Bindergen.useGenerators (["RadJavGenerator", "V8"]);
+		//embed.appendToCustomFile ("cmakeV8Includes", "${libRadJav_SOURCE_DIR}/include/RadJav/v8/RadJavV8GUIButton.h");
+		//embed.outputFilename ("RadJav%PASS%GUIButton");
+		let RadJavGUI = embed.useNamespace ("RadJav.GUI");
+		let GObject = RadJavGUI.getClass ("GObject");
+
+		class Button extends GObject
 		{
+			constructor ()
+			{
+			}
+
+			create ()
+			{
+				let func = this.createFunction ("create");
+				let engineArgs = this.$generator.getFunction ("RadJav.getEngineArgs");
+
+				func.code = `
+				CPP::GUI::Button *appObject = RJNEW CPP::GUI::Button(${engineArgs ()});
+				appObject->create();
+				${this._appObj.set ("appObject")};
+						`;
+				func.returns = this.$generator.callFunction ("RadJav.createPromise");
+
+				return (func);
+			}
+
+			setText (text)
+			{
+				let func = this.createFunction ("setText");
+
+				func.code = `
+				String str = ${text.toEngineString()};
+				UITYPE *appObject = (UITYPE *)${this.getExternal ("_appObj")};
+
+				if (appObject != NULL)
+					appObject->setText(str);
+						`;
+
+				return (func);
+			}
+
+			getText ()
+			{
+				let func = this.createFunction ("getText");
+				let toEngineString = this.$generator.getFunction ("RadJav.toEngineString");
+
+				func.code = `
+				String text = ${this.getString ("_text")});
+				UITYPE *appObject = (UITYPE *)${this.getExternal ("_appObj")};
+
+				if (appObject != NULL)
+					text = appObject->getText();
+						`;
+				func.returns = `${toEngineString ("text")}`;
+
+				return (func);
+			}
 		}
-
-		create ()
-		{
-			let func = this.createFunction ("create");
-			let engineArgs = this.$generator.getFunction ("RadJav.getEngineArgs");
-
-			func.code = `
-			CPP::GUI::Button *appObject = RJNEW CPP::GUI::Button(${engineArgs ()});
-			appObject->create();
-			${this._appObj.set ("appObject")};
-					`;
-			func.returns = this.$generator.callFunction ("RadJav.createPromise");
-
-			return (func);
-		}
-
-		setText (text)
-		{
-			let func = this.createFunction ("setText");
-
-			func.code = `
-			String str = ${text.toEngineString()};
-			UITYPE *appObject = (UITYPE *)${this.getExternal ("_appObj")};
-
-			if (appObject != NULL)
-				appObject->setText(str);
-					`;
-
-			return (func);
-		}
-
-		getText ()
-		{
-			let func = this.createFunction ("getText");
-			let toEngineString = this.$generator.getFunction ("RadJav.toEngineString");
-
-			func.code = `
-			String text = ${this.getString ("_text")});
-			UITYPE *appObject = (UITYPE *)${this.getExternal ("_appObj")};
-
-			if (appObject != NULL)
-				text = appObject->getText();
-					`;
-			func.returns = `${toEngineString ("text")}`;
-
-			return (func);
-		}
+		//embed.generate ();
 	}
-	//embed.generate ();
 
 	let generators = Bindergen.useGenerators (["AndroidJNI", "RadJavGenerator"]);
 	generators.parseFiles ([
